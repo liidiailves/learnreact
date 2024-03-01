81ac0301cfa957f0b5b1f86ab43d9a46ec15e91e
Liidia Ilves <liidia_ilves@hotmail.com>
2024-03-01 23:36:00 +0200

    Css example

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
 import "./App.css";
 
 function App() {
+  const age = 17;
+  const isGreen = true;
+
   return (
     <div className="App">
-      <User name="Liidia" age={25} email="liidia@example.com" />
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <User name="Jessica" age={24} email="jessica@example.com" />
-      <Job salary={12000} position="Junior SDE" company="Google" />
-      <User name="Jake" age={26} email="jake@example.com" />
-      <Job salary={10000} position="Project Manager" company="Netflix" />
+      {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+      <h2 style={{ color: isGreen ? "green" : "red" }}>THIS HAS COLOR</h2>
+
+      { isGreen && <button>THIS IS A BUTTON</button> }
     </div>
   );
 }
-
-// component must start with capital letter
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h2>{props.email}</h2>
-    </div>
-  );
-};
-
-const Job = (props) => {
-  return (
-    <div>
-      <h3>{props.salary}</h3>
-      <h3>{props.position}</h3>
-      <h3>{props.company}</h3>
-    </div>
-  );
-};
 
 export default App;
