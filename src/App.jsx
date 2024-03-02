aab74a0ba993dd0afe0054db721c7f04b6be90ef
Liidia Ilves <liidia_ilves@hotmail.com>
2024-03-02 22:14:00 +0200

    Add useState examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,54 @@
 import "./App.css";
-import { User } from './User.jsx';
+import { useState } from "react";
 
 function App() {
-  const users = [
-    { name: "Liidia", age: 25 },
-    { name: "Jake", age: 26 },
-    { name: "Jessica", age: 24 },
-  ]
+  const [age, setAge] = useState(0);
+  const increaseAge = () => {
+    setAge(age +1);
+  };
 
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Neptune", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-  ];
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const [showText, setShowText] = useState(true);
+
+  const [textColor, setTextColor] = useState("black");
+
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      { users.map((user, key) => {
-        return <User key={key} name={user.name} age={user.age} />
-      })}
-
-      { planets.map((planet, key) => !planet.isGasPlanet && <h2> {planet.name } </h2>
-      )}
+      {age} 
+      <button onClick={increaseAge}>Increase age</button>
+      <br />
+      <br />
+      <input type="text" onChange={handleInputChange} />
+      <br />
+      {inputValue}
+      <br />
+      <br />
+      <button onClick={() => {
+        setShowText(!showText)
+      }}>Show / Hide</button>
+      {showText && <h2>HI, MY NAME IS LIIDIA</h2>}
+      <br />
+      <button onClick={() => {
+        setTextColor (textColor === "black" ? "red" : "black");
+      }}>Change color</button>
+      <h2 style={{ color: textColor }}>HI, MY NAME IS LIIDIA</h2>
+      <br />
+      <button onClick={() => {
+        setCount(count + 1);
+      }}>Increase</button>
+      <button onClick={() => {
+        setCount(count - 1);
+      }}>Decrease</button>
+      <button onClick={() => {
+        setCount(0);
+      }}>Set to Zero</button>
+      {count}
     </div>
   );
 }
