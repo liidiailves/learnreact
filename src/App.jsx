3a76d30178c82a51901c73bc8ce6a62c742d40f0
Liidia Ilves <liidia_ilves@hotmail.com>
2024-03-04 12:10:50 +0200

    Add first example of fetching API: cat facts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import Axios from "axios";
 
 function App() {
-  
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  }
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
   return (
     <div className="App">
-
+      <button onClick={fetchCatFact}> Generate Cat Fact </button>
+      <p>{catFact}</p>
     </div>
   );
 }
