3875bb963574d2e01b9181141b8b3dbc2b7597e7
Liidia Ilves <liidia_ilves@hotmail.com>
2024-03-04 13:08:49 +0200

    Add second API example: the age of the name

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,35 @@ function App() {
     fetchCatFact();
   }, []);
 
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`)
+      .then((res) => {
+        setPredictedAge(res.data);
+    });
+  };
+
   return (
     <div className="App">
+      <div className="catFact">
       <button onClick={fetchCatFact}> Generate Cat Fact </button>
       <p>{catFact}</p>
+      </div>
+
+      <div className="nameAge">
+        <input 
+          placeholder="Submit your name"
+          onChange={(event) => {
+            setName(event.target.value);
+          }} />
+        <button onClick={fetchData}>Predict Age</button>
+        <p>Name: {predictedAge?.name}</p>
+        <p>Predicted Age: {predictedAge?.age}</p>
+        <p>Count: {predictedAge?.count}</p>
+      </div>
+      
     </div>
   );
 }
