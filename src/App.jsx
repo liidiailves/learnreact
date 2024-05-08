433a48451d69c9205aa80d54320809fa9238e3cd
Liidia Ilves <liidia_ilves@hotmail.com>
2024-05-08 15:30:13 +0300

    Implement newer solution of react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import WebApis from "./pages/WebApis";
 import Menu from "./pages/Menu";
@@ -7,20 +13,36 @@ import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/learnreact" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/learnreact/webapis" element={<WebApis />} />
+        <Route path="/learnreact/menu" element={<Menu />} />
+        <Route path="/learnreact/contact" element={<Contact />} />
+        <Route path="/learnreact/*" element={<h1>Page not found!</h1>} />
+      </Route>
+    )
+  );
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/learnreact" element={<Home />} />
-          <Route path="/learnreact/webapis" element={<WebApis />} />
-          <Route path="/learnreact/menu" element={<Menu />} />
-          <Route path="/learnreact/contact" element={<Contact />} />
-          <Route path="/learnreact/*" element={<h1>Page not found!</h1>} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </div>
   );
 }
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 export default App;
